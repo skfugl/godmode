f5d824b87822844e33ab7d13e22848ae67cb1c7c
skfugl <simon-kolind-fuglsang@hotmail.com>
2024-07-03 17:57:18 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+let currentIndex = 0;
+
+const slides = document.querySelectorAll('.slide');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+
+function showSlide(index) {
+    if (index >= slides.length) {
+        currentIndex = 0;
+    } else if (index < 0) {
+        currentIndex = slides.length - 1;
+    } else {
+        currentIndex = index;
+    }
+    const offset = -currentIndex * 100;
+    document.querySelector('.slider-content').style.transform = `translateX(${offset}%)`;
+}
+
+prevButton.addEventListener('click', () => {
+    showSlide(currentIndex - 1);
+});
+
+nextButton.addEventListener('click', () => {
+    showSlide(currentIndex + 1);
+});
+
+showSlide(currentIndex);
